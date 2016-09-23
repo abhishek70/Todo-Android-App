93d258ee103259998d5740ec8e8e50a4e9ca5408
Abhishek Desai <abhishek.desai70@gmail.com>
2016-09-22 22:52:48 -0700

    Added background colors to improve the app UI

--- app/src/main/java/com/example/abhishek/todoapp/Adapters/TodoListAdapter.java
+++ app/src/main/java/com/example/abhishek/todoapp/Adapters/TodoListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.abhishek.todoapp.Adapters;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
+
+import static android.webkit.WebSettings.RenderPriority.HIGH;
 
 
 /**
@@ -62,13 +65,39 @@ public class TodoListAdapter extends ArrayAdapter<TodoItem> {
         TextView todoNoteTv = (TextView) convertView.findViewById(R.id.todo_note_tv);
         todoNoteTv.setText(todoItem.getTodoNote());
 
+        // TodoPriority
         TextView todoPriorityTv = (TextView) convertView.findViewById(R.id.todo_priority_tv);
         todoPriorityTv.setText(todoItem.getTodoPriority());
 
+        // Setting different colors depending on the priority
+        int colorId = ContextCompat.getColor(getContext(),R.color.lowPriority);
+
+        switch (todoItem.getTodoPriority()){
+            case "HIGH":
+                colorId = ContextCompat.getColor(getContext(),R.color.highPriority);
+                break;
+
+            case "MEDIUM":
+                colorId = ContextCompat.getColor(getContext(),R.color.mediumPriority);
+                break;
+
+            case "LOW":
+                colorId = ContextCompat.getColor(getContext(),R.color.lowPriority);
+                break;
+
+            default:
+                colorId = ContextCompat.getColor(getContext(),R.color.lowPriority);
+                break;
+
+        }
+
+        todoPriorityTv.setTextColor(colorId);
+
+        // TodoDueDate
         TextView todoDueDateTv  = (TextView) convertView.findViewById(R.id.todo_due_date_tv);
 
-
         try {
+            // Converting the date into readable format
             String dateString = todoItem.getTodoDueDate();
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
             Date date = sdf.parse(dateString);
@@ -76,7 +105,7 @@ public class TodoListAdapter extends ArrayAdapter<TodoItem> {
             SimpleDateFormat outputFormat = new SimpleDateFormat("MMM d, yyyy", Locale.getDefault());
             String formattedDate = outputFormat.format(date);
 
-            todoDueDateTv.setText("Due : "+formattedDate);
+            todoDueDateTv.setText(getContext().getString(R.string.todo_due_date_label)+" "+formattedDate);
 
 
         } catch (ParseException e) {
